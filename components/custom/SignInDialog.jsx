d9a9ad0c5eb98c34734a920bcd7c07db03650905
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-02-24 15:15:49 +0530

    Add Convex schema for users table

--- components/custom/SignInDialog.jsx
+++ components/custom/SignInDialog.jsx
@@ -12,9 +12,13 @@ import { Button } from "../ui/button";
 import { useGoogleLogin } from "@react-oauth/google";
 import { UserDetailContext } from "@/context/UserDetailContext";
 import axios from "axios";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import uuid4 from "uuid4";
 
 function SignInDialog({ openDialog, closeDialog }) {
   const { userDetail, setUserDetail } = useContext(UserDetailContext);
+  const CreateUser=useMutation(api.users.CreateUsers)
 
   const googleLogin = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
@@ -25,6 +29,19 @@ function SignInDialog({ openDialog, closeDialog }) {
       );
 
       console.log(userInfo);
+      const user=userInfo.data
+      await CreateUser({
+        name:user?.name,
+        email:user?.email,
+        picture:user?.picture,
+        uid:uuid4()
+      })
+
+      if(typeof window!==undefined)
+      {
+        localStorage.setItem('user',JSON.stringify(user))
+      }
+
       setUserDetail(userInfo?.data);
       closeDialog(false);
     },
