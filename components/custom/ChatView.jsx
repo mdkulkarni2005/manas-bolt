629301aa6da5d7159eb1d615c9462b8db3a99ee8
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-02-24 22:43:53 +0530

    AI Generation done

--- components/custom/ChatView.jsx
+++ components/custom/ChatView.jsx
@@ -1,49 +1,119 @@
-"use client"
+"use client";
 
-import { MessagesContext } from '@/context/MessagesContext'
-import { UserDetailContext } from '@/context/UserDetailContext'
-import { api } from '@/convex/_generated/api'
-import Color from '@/data/Color'
-import Lookup from '@/data/Lookup'
-import { useConvex } from 'convex/react'
-import { ArrowRight, Link } from 'lucide-react'
-import Image from 'next/image'
-import { useParams } from 'next/navigation'
-import React, { useContext, useEffect, useState } from 'react'
+import { MessagesContext } from "@/context/MessagesContext";
+import { UserDetailContext } from "@/context/UserDetailContext";
+import { api } from "@/convex/_generated/api";
+import Color from "@/data/Color";
+import Lookup from "@/data/Lookup";
+import Prompt from "@/data/Prompt";
+import axios from "axios";
+import { useConvex, useMutation } from "convex/react";
+import { ArrowRight, Link, Loader2Icon } from "lucide-react";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import React, { useContext, useEffect, useState } from "react";
+import Markdown from "react-markdown";
+import ReactMarkdown from "react-markdown";
 
 function ChatView() {
-    const {id}=useParams()
-    const convex=useConvex()
-    const {userDetail,setUserDetail}=useContext(UserDetailContext)
-    const {messages,setMessages}=useContext(MessagesContext)
-    const [userInput,setUserInput]=useState()
+  const { id } = useParams();
+  const convex = useConvex();
+  const { userDetail, setUserDetail } = useContext(UserDetailContext);
+  const { messages, setMessages } = useContext(MessagesContext);
+  const [userInput, setUserInput] = useState();
+  const [loading, setLoading] = useState(false);
+  const UpdateMessages = useMutation(api.workspace.UpdateMessages);
 
-    useEffect(()=>{
-        id&&GetWorkspaceData()
-    },[id])
+  useEffect(() => {
+    id && GetWorkspaceData();
+  }, [id]);
 
-    const GetWorkspaceData=async()=>{
-        const result=await convex.query(api.workspace.GetWorkspace,{
-            workspaceId:id
-        })
-        setMessages(result?.messages)
-        console.log(result)
+  const GetWorkspaceData = async () => {
+    const result = await convex.query(api.workspace.GetWorkspace, {
+      workspaceId: id,
+    });
+    setMessages(result?.messages);
+    console.log(result);
+  };
+
+  useEffect(() => {
+    if (messages?.length > 0) {
+      const role = messages[messages?.length - 1].role;
+      if (role == "user") {
+        GetAiResponse();
+      }
     }
-  return (
-    <div className='relative h-[85vh] flex flex-col'>
-        <div className='flex-1 overflow-z-scroll'>
-            {messages?.map((msg,index)=>(
-                <div key={index} className='p-3 rounded-lg mb-2 flex gap-2 items-start' style={{
-                    backgroundColor:Color.CHAT_BACKGROUND
-                }}>
-                    {msg?.role=='user'&&<Image src={userDetail?.picture} alt='userImage' width={35} height={35} className='rounded-full' />}
-                    <h2>{msg.content}</h2>
-                </div>
-            ))}
-        </div>
+  }, [messages]);
 
-        {/* input section */}
-        <div
+  const GetAiResponse = async () => {
+    setLoading(true);
+    const PROMPT = JSON.stringify(messages) + Prompt.CHAT_PROMPT;
+    const result = await axios.post("/api/ai-chat", {
+      prompt: PROMPT,
+    });
+    const aiResp = {
+      role: "ai",
+      content: result.data.result,
+    };
+    setMessages((prev) => [...prev, aiResp]);
+    await UpdateMessages({
+      messages: [...messages, aiResp],
+      workspaceId: id,
+    });
+    setLoading(false);
+  };
+
+  const onGenerate = (input) => {
+    setMessages((prev) => [
+      ...prev,
+      {
+        role: "user",
+        content: input,
+      },
+    ]);
+    setUserInput("");
+  };
+
+  return (
+    <div className="relative h-[85vh] flex flex-col">
+      <div className="flex-1 overflow-y-scroll scrollbar-hide">
+        {messages?.map((msg, index) => (
+          <div
+            key={index}
+            className="p-3 rounded-lg mb-2 flex gap-2 items-start leading-7"
+            style={{
+              backgroundColor: Color.CHAT_BACKGROUND,
+            }}
+          >
+            {msg?.role == "user" && (
+              <Image
+                src={userDetail?.picture}
+                alt="userImage"
+                width={35}
+                height={35}
+                className="rounded-full"
+              />
+            )}
+            <div className="markdown-body flex flex-col">
+              <Markdown>{msg.content}</Markdown>
+            </div>
+          </div>
+        ))}
+        {loading && (
+          <div
+            className="p-3 rounded-lg mb-2 flex gap-2 items-center"
+            style={{
+              backgroundColor: Color.CHAT_BACKGROUND,
+            }}
+          >
+            <Loader2Icon className="animate-spin" />
+            <h2>Generating response...</h2>
+          </div>
+        )}
+      </div>
+
+      {/* input section */}
+      <div
         className="p-5 border rounded-xl max-w-xl w-full mt-3"
         style={{
           backgroundColor: Color.BACKGROUND,
@@ -51,6 +121,7 @@ function ChatView() {
       >
         <div className="flex gap-3">
           <textarea
+            value={userInput}
             placeholder={Lookup.INPUT_PLACEHOLDER}
             onChange={(event) => setUserInput(event.target.value)}
             className="outline-none bg-transparent w-full h-32 max-h-56 resize-none"
@@ -67,7 +138,7 @@ function ChatView() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ChatView
+export default ChatView;
