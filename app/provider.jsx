19a777ae062959389874d912aba0da8698d3864a
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-02-24 10:44:49 +0530

    Landing Page Done

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,18 +1,24 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import Header from "@/components/custom/Header";
+import { MessageContext } from "@/context/MessagesContext";
 function Provider({ children }) {
+  const [message,setMessage] = useState()
   return (
     <div>
-      <NextThemesProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </NextThemesProvider>
+      <MessageContext.Provider value={{message,setMessage}}>
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+        </NextThemesProvider>
+      </MessageContext.Provider>
     </div>
   );
 }
---
+++ components/custom/Hero.jsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { MessageContext } from "@/context/MessagesContext";
+import Color from "@/data/Color";
+import Lookup from "@/data/Lookup";
+import { ArrowRight, Link, Link2 } from "lucide-react";
+import React, { useContext, useState } from "react";
+
+function Hero() {
+  const [userInput, setUserInput] = useState();
+  const { message, setMessage } = useContext(MessageContext);
+
+  const onGenerate = (input) => {
+    setMessage({
+      role: "user",
+      content: input,
+    });
+  };
+
+  return (
+    <div className="flex flex-col items-center mt-36 xl:mt-52 gap-2">
+      <h2 className="font-bold text-4xl">{Lookup.HERO_HEADING}</h2>
+      <p className="text-gray-400 font-medium">{Lookup.HERO_DESC}</p>
+      <div
+        className="p-5 border rounded-xl max-w-xl w-full mt-3"
+        style={{
+          backgroundColor: Color.BACKGROUND,
+        }}
+      >
+        <div className="flex gap-3">
+          <textarea
+            placeholder={Lookup.INPUT_PLACEHOLDER}
+            onChange={(event) => setUserInput(event.target.value)}
+            className="outline-none bg-transparent w-full h-32 max-h-56 resize-none"
+          />
+          {userInput && (
+            <ArrowRight
+              onClick={() => onGenerate(userInput)}
+              className="bg-blue-500 p-2 h-10 w-10 rounded-md cursor-pointer"
+            />
+          )}
+        </div>
+        <div>
+          <Link className="h-5 w-5" />
+        </div>
+      </div>
+
+      <div className="flex mt-8 flex-wrap max-w-2xl items-center justify-center gap-3">
+        {Lookup?.SUGGSTIONS.map((suggestion, index) => (
+          <h2
+            className="p-1 px-2 border rounded-full text-sm text-gray-400 hover:text-white cursor-pointer"
+            key={index}
+            onClick={()=>onGenerate(suggestion)}
+          >
+            {suggestion}
+          </h2>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
